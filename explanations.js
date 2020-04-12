1768fcc2e6de10d58da1ffeddb5dc6dbe37f502c
tien-nguyen-dev <62980048+tien-nguyen-dev@users.noreply.github.com>
2020-04-12 09:35:56 +0700

    write some functions

---
+++ explanations.js
@@ -0,0 +1,60 @@
+//Explanations
+var favColor = 'red';
+var myArray = ['blue', 'red', 'green'];
+var numOfFavColor = '3';
+var hasGotFavColor = false; 
+var tiensObject = {
+	firstName: 'Tien',
+	lastName: 'Nguyen',
+	favColors: ['black', 'yellow'],
+	ageOfConsent: 21,
+	isFemale: true
+};
+
+
+var whatIsMyFavColor = function () {
+	return false;
+	return true;
+	return 'blue'; 
+}
+var doSomething = function () {
+	console.log( "doSomething!!!");
+}
+var area = function (width, height) {
+	return width*height;
+}
+var fullname = function (firstname, secondname) {
+	return firstname + ' ' + secondname;
+}
+
+// If Else 
+if (true) {
+	// do sth
+	alert ('meow');
+};
+
+if ('meow' == 'meow') {
+	alert ('meow');
+};
+
+var no1 = 10;
+var no2 = 100;
+if (no1 > no2){
+	alert ('meow');
+}
+else {
+	alert ('meowww');
+};
+
+var name1 = 'tien';
+var name2 = 'nguyen';
+if (name1 == 'tien' || name2 == 'nguyen') {
+	alert ('true!!!');
+	}
+	else if (name2 == 'tien') {
+		alert ('true');
+	} 
+	else {
+		alert ('false');
+	}
+

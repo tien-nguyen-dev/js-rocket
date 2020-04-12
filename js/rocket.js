9e506b4ea6566ead419f40d1de90a3a9e8e064e8
tien-nguyen-dev <62980048+tien-nguyen-dev@users.noreply.github.com>
2020-04-12 23:28:46 +0700

    Create rocket.js

---
+++ js/rocket.js
@@ -0,0 +1,14 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+	document.body.className = 'body-state'+ state;}
+
+	if (state == 2) {
+		timer = setInterval(function () {
+			document.getElementById('countdown').innerHTML = countdownNumber;
+		countdownNumber = countdownNumber - 1;
+	}, 1000);
+	
+
+};
